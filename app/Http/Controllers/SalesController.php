90c1a24e12f04b64c73b2ec8141e0dff9664ba85
zozors <romance.dawn1989@gmail.com>
2023-10-29 23:17:19 +0900

    検索からの削除処理、salesコントローラートランザクションエラー解消

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Product;
 use App\Models\Sale;
 
@@ -10,25 +11,36 @@ class SalesController extends Controller
 {
     public function purchase(Request $request)
     {
-        $productId = $request->input('product_id');
-        $quantity = $request->input('quantity', 1);
-        $product = Product::find($productId);
+        return DB::transaction(function() use ($request){
 
-        if (!$product) {
-            return response()->json(['message' => '商品が存在しません'], 404);
-        }
-        if ($product->stock < $quantity) {
-            return response()->json(['message' => '商品が在庫不足です'], 400);
-        }
+            $productId = $request->input('product_id');
+            $quantity = $request->input('quantity', 1);
+            $product = Product::find($productId);
 
-        $product->stock -= $quantity;
-        $product->save();
-        $sale = new Sale([
-            'product_id' => $productId,
-        ]);
+            if (!$product) {
+                return response()->json(['message' => '商品が存在しません'], 404);
+            }
+            if ($product->stock < $quantity) {
+                return response()->json(['message' => '商品が在庫不足です'], 400);
+            }
 
-        $sale->save();
+            try {
+                // トランザクション内で在庫減少と売上レコードの追加を行う
+                $product->stock -= $quantity;
+                $product->save();
+                $sale = new Sale([
+                    'product_id' => $productId,
+                ]);
+                $sale->save();
+            } catch (Exception $e) {
+                // 何らかのエラーが発生した場合、トランザクションをロールバック
+                DB::rollBack();
+                return response()->json(['message' => '購入に失敗しました'], 500);
+            }
 
-        return response()->json(['message' => '購入成功']);
+            // トランザクションが正常に終了した場合、コミット
+            DB::commit();
+            return response()->json(['message' => '購入成功']);
+        });
     }
 }
--- resources/js/products.js
+++ resources/js/products.js
@@ -1,5 +1,6 @@
-$(document).ready(function () {
-
+$(document).ready(function () { //HTMLを読み込んだら削除ボタンの機能を読み込みます(3行目)
+    console.log('Hello');
+    deleteProduct();
     $("#product-table th a").on("click", function (e) {
         e.preventDefault();
         var sortColumn = $(this).data("sort");
@@ -13,48 +14,52 @@ $(document).ready(function () {
         loadProductList();
     });
 
-    function loadProductList(sortColumn = 'id', sortDirection = 'asc') {
+    function loadProductList(sortColumn = 'id', sortDirection = 'asc') { //検索ボタンを押した時の動作
 
         $.ajax({
             url: productListUrl,
             method: "GET",
-
             data: $('#search-form').serialize(),
             dataType: "html",
-            success: function (data) {
-                let newtable = $(data).find('#product-list')
-                $("#product-list").html(newtable);
-            },
-            error: function (xhr) {
-                console.error(xhr);
-            },
+        })
+        .done(function (data) {
+            let newtable = $(data).find('#product-list');
+            $("#product-list").html(newtable);
+            deleteProduct();
+        })
+        .fail(function (xhr) {
+            console.error(xhr);
         });
+        console.log('検索成功');
     }
+});
 
-    $('.delete-product').on("click", function (e) {
+function deleteProduct() {
+    $('.delete-product').on('click',function(e){
         e.preventDefault();
         var productID = $(this).data('product-id');
         var deleteUrl = $(this).data('delete-url');
         var deleteConfirm = confirm('削除してもよろしいでしょうか?');
-
+    
         if (deleteConfirm) {
             var clickEle = $(this);
-
+    
             $.ajax({
                 headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
                 url: deleteUrl,
                 type: 'DELETE',
                 data: { 'id': productID, '_method': 'DELETE' },
             })
-
+    
             .done(function() {
                 var productRow = clickEle.closest('tr');
                 productRow.remove();
+                console.log('削除成功');
             })
-
+    
             .fail(function() {
                 alert('エラー');
             });
         }
-    });
-});
+    })
+}
